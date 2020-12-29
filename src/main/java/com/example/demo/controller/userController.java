fbe00a59d74122efb8c0ae4fdd65abbc26abd8a9
sakura <154688796@qq.com>
2020-12-29 22:44:10 +0800

    mybatis-plus

--- src/main/java/com/example/demo/controller/userController.java
+++ src/main/java/com/example/demo/controller/userController.java
@@ -1,6 +1,9 @@
 package com.example.demo.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.demo.mapper.UserMapper;
 import com.example.demo.po.User;
 import com.example.demo.service.UserService;
@@ -17,17 +20,63 @@ public class userController {
     private UserService userService;
     @Resource
     private UserMapper userMapper;
+    User user = new User();
 
     @GetMapping("/{id}")
     public Object getone(@PathVariable("id")int id) {
         QueryWrapper<User> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq("id",id);
-
         User user = userService.getOne(queryWrapper);
         return user;
     }
+    @GetMapping("/all")
+    public Object selectMaps(){
+        return userMapper.selectMaps(new QueryWrapper<User>().isNotNull("name"));
+    }
+    @GetMapping("/page")//查询没问题 显示有问题
+    public Object selectPage(){
+        Page<User> page = new Page<>(1, 2);
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        IPage<User> userIPage = userMapper.selectPage(page, queryWrapper);
+        return userIPage;
+    }
     @GetMapping("/po/{id}")
-    public Object selectById( int id){
-        return userMapper.selectById(new QueryWrapper<User>().eq("id",id));
+    public Object selectOne(@PathVariable("id")int id){
+        QueryWrapper<User> queryWrapper = new QueryWrapper<User>();
+        return userMapper.selectOne(queryWrapper.eq("id",id));
+    }
+    @GetMapping("/delect")
+    public Object delect(){
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper
+                .isNotNull("name")
+                .ge("age", 45);
+        userMapper.delete(queryWrapper);
+        return null;
+    }
+    @GetMapping("/update")
+    public Object update(){
+        //修改值
+        user.setName("六六");
+        user.setAge(35);
+
+        //修改条件s
+        UpdateWrapper<User> userUpdateWrapper = new UpdateWrapper<>();
+        userUpdateWrapper.eq("name", "张三");
+        userMapper.update(user, userUpdateWrapper);
+        return null;
+    }
+    @GetMapping("/insert")
+    public Object insert(){
+        //修改值
+
+        user.setName("六六");
+        user.setAge(35);
+
+        //修改条件s
+        UpdateWrapper<User> userUpdateWrapper = new UpdateWrapper<>();
+
+        userMapper.insert(user);
+        return null;
     }
 }
